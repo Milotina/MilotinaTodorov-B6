db36c18aa045eef100cde622d9b4e17c8ddde090
milot <milot@DESKTOP-1PJF4EO>
2021-12-21 22:22:03 -0500

    test

--- src/test/java/com/noorteck/java/hw6/Day6Demo.java
+++ src/test/java/com/noorteck/java/hw6/Day6Demo.java
@@ -3,7 +3,7 @@ package com.noorteck.java.hw6;
 public class Day6Demo {
 	
 	public static void main(String[] args) {
-		System.out.println("Test 101...");
+		System.out.println("Test 101......");
 	}
 
 }
