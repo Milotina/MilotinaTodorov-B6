adef892b4d40a39cf84bd1bb0eec8806eecbd888
milot <milot@DESKTOP-1PJF4EO>
2022-01-01 15:15:32 -0500

    Day 8 Homework - Question 3

--- src/test/java/com/noorteck/java/hw8/QuestionThree.java
+++ src/test/java/com/noorteck/java/hw8/QuestionThree.java
@@ -4,34 +4,34 @@ public class QuestionThree {
 	public static void main(String[] args) {
 
 		// FOR LOOP
-		int i = 21;
-		int average = 0;
+		int i = 20;
+		int sum = 0;
 
-		for (i = 21; i <= 29; i++) {
-			average = average + i;
+		for (i = 20; i <= 30; i++) {
+			sum = sum + i;
 		}
-		System.out.println("Average of numbers between 20-30 is " + average / 9);
+		System.out.println("Average of numbers between 20-30 is " + sum / 11);
 		System.out.println("**************************************");
 
 		// WHILE LOOP
-		int x = 21;
-		int average2 = 0;
+		int x = 20;
+		int sum2 = 0;
 
-		while (x <= 29) {
-			average2 = average2 + x;
+		while (x <= 30) {
+			sum2 = sum2 + x;
 			x++;
 		}
-		System.out.println("Average of numbers between 20-30 is " + average2 / 9);
+		System.out.println("Average of numbers between 20-30 is " + sum2 / 11);
 		System.out.println("**************************************");
 
 		// DO WHILE LOOP
-		int y = 21;
-		int average3 = 0;
+		int y = 20;
+		int sum3 = 0;
 
 		do {
-			average3 = average3 + y;
+			sum3 = sum3 + y;
 			y++;
-		} while (y <= 29);
-		System.out.println("Average of numbers between 20-30 is " + average3 / 9);
+		} while (y <= 30);
+		System.out.println("Average of numbers between 20-30 is " + sum3 / 11);
 	}
 }
