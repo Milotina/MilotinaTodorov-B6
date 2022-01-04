950e04f204db654a1c8d2cbe83e8363b1092f7bd
milot <milot@DESKTOP-1PJF4EO>
2022-01-04 15:30:02 -0500

    Day 10 Homework - Methods

--- src/test/java/com/noorteck/java/hw10/Question3.java
+++ src/test/java/com/noorteck/java/hw10/Question3.java
@@ -8,9 +8,9 @@ public class Question3 {
 
 	public static void oddEven(int number) {
 		if (number % 2 == 0) {
-			System.out.println(number + " is an even number.");
+			System.out.println(number + " is an even number");
 		} else {
-			System.out.println(number + " is an odd number.");
+			System.out.println(number + " is an odd number");
 		}
 	}
 
