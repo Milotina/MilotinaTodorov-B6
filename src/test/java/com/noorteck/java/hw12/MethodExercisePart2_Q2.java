885da73e53d768983d98f6f980cb59b17c1c329d
milot <milot@DESKTOP-1PJF4EO>
2022-01-08 10:17:40 -0500

    Day 12 Homework - Methods

--- src/test/java/com/noorteck/java/hw12/MethodExercisePart2_Q2.java
+++ src/test/java/com/noorteck/java/hw12/MethodExercisePart2_Q2.java
@@ -17,7 +17,7 @@ public class MethodExercisePart2_Q2 {
 	public static boolean multipleOf(int num) {
 		boolean result = false;
 
-		if (num % 3 == 0 || num % 5 == 0) {
+		if (num > 0 && (num % 3 == 0 || num % 5 == 0)) {
 			result = true;
 		}
 		return result;
